b5caba1db0f35a8b095ac1f833712721c4586bb7
-T.K.- <tk_dandelion@126.com>
2018-09-10 17:54:24 +0800

    update drive_with_joystick

--- examples/drive_with_joystick.py
+++ examples/drive_with_joystick.py
@@ -6,8 +6,7 @@ from cosmoz.modules import tank_drive
 joysticks = XBoxController.init_all()
 joystick_0 = joysticks[0]
 
-keyboard = Keyboard(suppress=True)
-board = Arduino_UNO(keyboard)
+board = Arduino_UNO(joystick_0)
 
 while True:
     tank_drive(board, 6, 8, 5, 7, joystick_0)
